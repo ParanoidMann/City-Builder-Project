9a15c4e431d90375366a74c195d13d750016d9f9
Nikolai Rodin <mashyrinwork@gmail.com>
2020-12-05 21:24:54 +0300

    [dev] Added houses height

---
+++ Assets/_Project/Scripts/City/Builders/Terrain/BuildingCreator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Zenject;
+using UnityEngine;
+
+using _Project.Scripts.PrefabDictionary;
+using _Project.Scripts.City.ConfigWrappers;
+
+namespace _Project.Scripts.City
+{
+    public class BuildingCreator
+    {
+        private PrefabSerializableDictionary _prefabDictionary;
+
+        [Inject]
+        private BuildingCreator(PrefabSerializableDictionary prefabDictionary)
+        {
+            _prefabDictionary = prefabDictionary;
+        }
+
+        public GameObject CreateBuilding(Vector3Int position, BuildingConfig buildingConfig)
+        {
+            if (_prefabDictionary.TryGetValue(buildingConfig.PrefabId, out var buildingPrefab))
+            {
+                var building = MonoBehaviour.Instantiate(buildingPrefab);
+                building.transform.position = position;
+
+                return building;
+            }
+
+            throw new InvalidOperationException("No such element");
+        }
+    }
+}
