2b9f64eeb678585741712e5f65fc0365366da1ea
Nikolai Rodin <mashyrinwork@gmail.com>
2020-12-06 18:34:04 +0300

    [dev] added new buildings to grid

--- Assets/_Project/Scripts/City/Builders/Grid/GridBuilder.cs
+++ Assets/_Project/Scripts/City/Builders/Grid/GridBuilder.cs
@@ -6,6 +6,8 @@ namespace _Project.Scripts.City.Builders.Grid
 {
     public class GridBuilder : ICityBuilder
     {
+        private const int EmptyCellsOffset = 1;
+
         private CityConfig _cityConfig;
 
         private CityGrid _cityGrid;
@@ -23,7 +25,24 @@ namespace _Project.Scripts.City.Builders.Grid
 
         public void PlaceBuilding(Vector3Int position, int buildingIndex)
         {
-            _cityGrid[position.x, position.z] = CellType.Building;
+            var building = _cityConfig.Buildings[buildingIndex];
+            var baseSize = building.BaseSize;
+
+            for (var x = position.x - EmptyCellsOffset; x <= position.x + baseSize; x++)
+            {
+                for (var z = position.z - EmptyCellsOffset; z <= position.z + baseSize; z++)
+                {
+                    if (x < position.x || x >= position.x + baseSize ||
+                        z < position.z || z >= position.z + baseSize)
+                    {
+                        _cityGrid[x, z] = CellType.Closed;
+                    }
+                    else
+                    {
+                        _cityGrid[x, z] = CellType.Building;
+                    }
+                }
+            }
         }
 
         public bool IsPositionFree(Vector3Int position)
